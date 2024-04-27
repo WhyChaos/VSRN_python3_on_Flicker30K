2d285c15b1a96a66ba0f72a10e2190a5641eafc2
hc <nowadayshc@163.com>
2024-04-27 23:03:09 +0800

    .

--- models/EncoderRNN.py
+++ models/EncoderRNN.py
@@ -30,8 +30,7 @@ class EncoderRNN(nn.Module):
         elif rnn_cell.lower() == 'gru':
             self.rnn_cell = nn.GRU
 
-        self.rnn = self.rnn_cell(dim_hidden, dim_hidden, n_layers, batch_first=True,
-                                bidirectional=bidirectional, dropout=self.rnn_dropout_p)
+        self.rnn = self.rnn_cell(dim_hidden, dim_hidden, n_layers, batch_first=True, dropout=self.rnn_dropout_p)
 
         self._init_hidden()
 
